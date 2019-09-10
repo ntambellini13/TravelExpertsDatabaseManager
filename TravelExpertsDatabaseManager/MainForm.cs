200eb3d428b7bea0fadd8a294b643296971e7c07
Tarik <tnabdall@ucalgary.ca>
2019-09-10 14:47:36 -0600

    ui set up. combo box not working yet.

--- TravelExpertsDatabaseManager/MainForm.cs
+++ TravelExpertsDatabaseManager/MainForm.cs
@@ -7,14 +7,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TravelExpertsData;
 
 namespace TravelExpertsDatabaseManager
 {
     public partial class MainForm : Form
     {
+        public List<Package> packages;
+
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            InitializePackageDataBinding();
+        }
+
+        private void InitializePackageDataBinding()
+        {
+            packages = PackagesDB.GetPackages();
+            packageBindingSource.DataSource = packages;
+        }
+
+        private void packageIdComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Package selectedPackage = null;
+            for(int i = 0 ; i< packages.Count() ; i++)
+            {
+                if(packages[i].PackageId == (int) packageIdComboBox.SelectedValue)
+                {
+                    selectedPackage = packages[i];
+                    break;
+                }
+            }
+            if (selectedPackage != null)
+            {
+                airfairInclusionCheckBox.Checked = selectedPackage.AirfairInclusion;
+                imagePathTextBox.Text = selectedPackage.ImagePath;
+                packageAgencyCommissionTextBox.Text = selectedPackage.PackageAgencyCommission.ToString("c2");
+                packageBasePriceTextBox.Text = selectedPackage.PackageBasePrice.ToString("c2");
+                packageDescriptionTextBox.Text = selectedPackage.PackageDescription;
+                packageEndDateDateTimePicker.Value = selectedPackage.PackageEndDate;
+                packageNameTextBox.Text = selectedPackage.PackageName;
+                packageStartDateDateTimePicker.Value = selectedPackage.PackageStartDate;
+                partnerURLTextBox.Text = selectedPackage.PartnerURL;
+            }
         }
     }
 }
