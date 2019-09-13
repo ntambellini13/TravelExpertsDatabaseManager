e4f09d4daba463b246fe7e41aeeeaa996a7f0d6e
Tarik <tnabdall@ucalgary.ca>
2019-09-13 12:47:26 -0600

    Got rid of weird folder. I hope this works.

--- TravelExpertsData/Package.cs
+++ TravelExpertsData/Package.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Drawing;
+
 
 namespace TravelExpertsData
 {
     public class Package
     {
+        /// <summary>
+        /// Package ID
+        /// </summary>
         public int PackageId { get; set; }
+        /// <summary>
+        /// Package name. Can't be null/empty.
+        /// </summary>
         public string PackageName
         {
             get
@@ -27,24 +36,29 @@ namespace TravelExpertsData
                 }
             }
         }
-        public string ImagePath
-        {
+        /// <summary>
+        /// Image stored as array of bytes. Can't be null.
+        /// </summary>
+        public byte[] Image {
             get
             {
-                return imagePath;
+                return image;
             }
             set
             {
-                if (String.IsNullOrEmpty(value))
+                if (value == null)
                 {
                     throw new ArgumentNullException(nameof(value));
                 }
                 else
                 {
-                    imagePath = value;
+                    image = value;
                 }
             }
         }
+        /// <summary>
+        /// URL of partner site. Can't be null/empty.
+        /// </summary>
         public string PartnerURL
         {
             get
@@ -63,9 +77,21 @@ namespace TravelExpertsData
                 }
             }
         }
+        /// <summary>
+        /// Is airfair included in price?
+        /// </summary>
         public bool AirfairInclusion { get; set; }
-        public DateTime PackageStartDate { get; set; }
-        public DateTime PackageEndDate { get; set; }
+        /// <summary>
+        /// The start date of the package
+        /// </summary>
+        public DateTime PackageStartDate { get { return packageStartDate; } }
+        /// <summary>
+        /// The end date of the package
+        /// </summary>
+        public DateTime PackageEndDate { get { return packageEndDate; } }
+        /// <summary>
+        /// Package description. Can't be null/empty.
+        /// </summary>
         public string PackageDescription
         {
             get
@@ -84,26 +110,112 @@ namespace TravelExpertsData
                 }
             }
         }
+        /// <summary>
+        /// The base price of the package
+        /// </summary>
         public decimal PackageBasePrice { get; set; }
-        public decimal PackageAgencyCommission { get; set; }
+        /// <summary>
+        /// The commission the agency receives for the package.
+        /// </summary>
+        public decimal PackageAgencyCommission
+        {
+            get
+            {
+                return packageAgencyCommission;
+            }
+            set
+            {
+                if (value >= PackageBasePrice)
+                {
+                    throw new ArgumentOutOfRangeException("Commission must be less than base price!");
+                }
+                else
+                {
+                    packageAgencyCommission = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Displays the image data in a format that the picture box can use
+        /// </summary>
+        public Image ImageForPictureBox
+        {
+            get
+            {
+                return Image==null?  null: ByteToImage(Image);
+            }
+        }
 
         private string packageName;
-        private string imagePath;
         private string partnerURL;
         private string packageDescription;
+        private byte[] image;
+        private DateTime packageStartDate;
+        private DateTime packageEndDate;
+        private decimal packageAgencyCommission;
 
-        public Package(int packageId, string packageName, string imagePath, string partnerURL, bool airfairInclusion, DateTime packageStartDate, DateTime packageEndDate, string packageDescription, decimal packageBasePrice, decimal packageAgencyCommission)
+    
+        /// <summary>
+        /// Creates package
+        /// </summary>
+        /// <param name="packageId">id</param>
+        /// <param name="packageName">name</param>
+        /// <param name="image">byte array of image data</param>
+        /// <param name="partnerURL">partner url</param>
+        /// <param name="airfairInclusion">is airfair included?</param>
+        /// <param name="packageStartDate">pkg start date. should be before end date.</param>
+        /// <param name="packageEndDate">package end date. should be after start date.</param>
+        /// <param name="packageDescription">package description</param>
+        /// <param name="packageBasePrice">base price</param>
+        /// <param name="packageAgencyCommission">agency commission. should be less than base price.</param>
+        public Package(int packageId, string packageName, byte[] image, string partnerURL, bool airfairInclusion, DateTime packageStartDate, DateTime packageEndDate, string packageDescription, decimal packageBasePrice, decimal packageAgencyCommission)
         {
             PackageId = packageId;
             this.packageName = packageName ?? throw new ArgumentNullException(nameof(packageName));
-            ImagePath = imagePath ?? throw new ArgumentNullException(nameof(imagePath));
+            Image = image ?? throw new ArgumentNullException(nameof(image));
             PartnerURL = partnerURL ?? throw new ArgumentNullException(nameof(partnerURL));
             AirfairInclusion = airfairInclusion;
-            PackageStartDate = packageStartDate;
-            PackageEndDate = packageEndDate;
+            setPackageDates(packageStartDate, packageEndDate);
             PackageDescription = packageDescription ?? throw new ArgumentNullException(nameof(packageDescription));
             PackageBasePrice = packageBasePrice;
             PackageAgencyCommission = packageAgencyCommission;
         }
+
+        /// <summary>
+        /// Method to convert image byte array to Bitmap image
+        /// </summary>
+        /// <param name="blob">image byte[] array/param>
+        /// <returns>Image as Bitmap</returns>
+        public static Bitmap ByteToImage(byte[] blob)
+        {
+            if (blob == null) { throw new ArgumentNullException("Must supply blob array"); }
+            MemoryStream mStream = new MemoryStream();
+            byte[] pData = blob;            
+            mStream.Write(pData, 0, Convert.ToInt32(pData.Length));
+            Bitmap bm = new Bitmap(mStream, false);
+            mStream.Dispose();
+            return bm;
+        }
+
+        /// <summary>
+        /// Sets the package dates together ensuring that the start date is before the end date.
+        /// </summary>
+        /// <param name="startDate">Start date</param>
+        /// <param name="endDate">End date</param>
+        public void setPackageDates(DateTime startDate, DateTime endDate)
+        {
+            if (startDate >= endDate)
+            {
+                throw new ArgumentOutOfRangeException("Your start date must be before your end date.");
+            }
+            else
+            {
+                packageStartDate = startDate;
+                packageEndDate = endDate;
+            }
+        }
+
+
     }
 }
--- TravelExpertsData/PackagesDB.cs
+++ TravelExpertsData/PackagesDB.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Configuration;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +13,239 @@ namespace TravelExpertsData
 {
     public static class PackagesDB
     {
+        /// <summary>
+        /// Gets all the package ids in the db
+        /// </summary>
+        /// <returns>List of package ids</returns>
+        public static List<int> GetPackageIds()
+        {
+            List<int> packageIds = new List<int>(); // Empty list
+            int packageId; // for reading
+
+            // Opens connection
+            using (SqlConnection connection = TravelExpertsDB.GetConnection())
+            {
+                string query = "SELECT PackageId " +
+                               "FROM Packages " +
+                               "ORDER BY PackageId";
+                // Creates command
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+                {
+                    // Runs command
+                    connection.Open();
+                    SqlDataReader reader =
+                        cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                    // Reads and adds all package ids to the list
+                    while (reader.Read())
+                    {
+                        packageId = (int) reader["PackageId"];
+                        packageIds.Add(packageId); // add to the list
+                    }
+                } // cmd object recycled
+            }// connection object recycled
+            return packageIds;
+        }
+
+        /// <summary>
+        /// Gets a list of all packages in the DB
+        /// </summary>
+        /// <returns>List of packages</returns>
+        public static List<Package> GetPackages()
+        {
+            List<Package> packages = new List<Package>(); // Empty list
+            Package package; // for reading
+
+            // Opens connection
+            using (SqlConnection connection = TravelExpertsDB.GetConnection())
+            {
+                string query = "SELECT * " +
+                               "FROM Packages " +
+                               "ORDER BY PackageId";
+                // Creates command
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+                    SqlDataReader reader =
+                        cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                    // Reads all data into new package and adds to list
+                    while (reader.Read())
+                    {
+                        // Create new package with current row information
+                        package = new Package
+                            ((int) reader["PackageId"],
+                            reader["PkgName"].ToString(),
+                            (byte[]) reader["Image"],
+                            reader["Partner"].ToString(),
+                            ((int) reader["AirfairInclusion"]==1),
+                            (DateTime) reader["PkgStartDate"],
+                            (DateTime) reader["PkgEndDate"],
+                            reader["PkgDesc"].ToString(),
+                            (decimal) reader["PkgBasePrice"],
+                            (decimal) reader["PkgAgencyCommission"]);// empty invoice object
+                        
+                        packages.Add(package); // add to the list
+                    }
+                } // cmd object recycled
+            }// connection object recycled
+            return packages;
+        }
+
+        /// <summary>
+        /// Updates the specified package. Ensures no concurrency error by checking that it will only update if the package has not changed.
+        /// </summary>
+        /// <param name="oldPackage">Package info that program currently has</param>
+        /// <param name="newPackage">New package info</param>
+        /// <returns>Successful?</returns>
+        public static bool UpdatePackage(Package oldPackage, Package newPackage)
+        {
+            bool success;
+            // Opens connection
+            using (SqlConnection connection = TravelExpertsDB.GetConnection())
+            {
+                string query =
+                    "UPDATE Packages " +
+                    "SET " +
+                    "PkgName = @NewPkgName, " +
+                    "Image = @NewImage, " +
+                    "Partner = @NewPartner, " +
+                    "AirfairInclusion = @NewAirfairInclusion, " +
+                    "PkgStartDate = @NewPkgStartDate, " +
+                    "PkgEndDate = @NewPkgEndDate, " +
+                    "PkgDesc = @NewPkgDesc, " +
+                    "PkgBasePrice = @NewPkgBasePrice, " +
+                    "PkgAgencyCommission = @NewPkgAgencyCommission " +
+                    "WHERE " +
+                    "PackageId = @OldPackageId AND " +
+                    "PkgName = @OldPkgName AND " +
+                    "Image = @OldImage AND " +
+                    "Partner = @OldPartner AND " +
+                    "AirfairInclusion = @OldAirfairInclusion AND " +
+                    "PkgStartDate = @OldPkgStartDate AND " +
+                    "PkgEndDate = @OldPkgEndDate AND " +
+                    "PkgDesc = @OldPkgDesc AND " +
+                    "PkgBasePrice = @OldPkgBasePrice AND " +
+                    "PkgAgencyCommission = @OldPkgAgencyCommission ; ";
+
+                // Creates command and adds all proper parameters
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+
+                    cmd.Parameters.AddWithValue("@NewPkgName", newPackage.PackageName);
+                    cmd.Parameters.AddWithValue("@NewImage", newPackage.Image);
+                    cmd.Parameters.AddWithValue("@NewPartner", newPackage.PartnerURL);
+                    cmd.Parameters.AddWithValue("@NewAirfairInclusion", newPackage.AirfairInclusion ? 1 : 0);
+                    cmd.Parameters.AddWithValue("@NewPkgStartDate", newPackage.PackageStartDate);
+                    cmd.Parameters.AddWithValue("@NewPkgEndDate", newPackage.PackageEndDate);
+                    cmd.Parameters.AddWithValue("@NewPkgDesc", newPackage.PackageDescription);
+                    cmd.Parameters.AddWithValue("@NewPkgBasePrice", newPackage.PackageBasePrice);
+                    cmd.Parameters.AddWithValue("@NewPkgAgencyCommission", newPackage.PackageAgencyCommission);
+
+                    cmd.Parameters.AddWithValue("@OldPackageId", oldPackage.PackageId);
+                    cmd.Parameters.AddWithValue("@OldPkgName", oldPackage.PackageName);                  
+                    cmd.Parameters.AddWithValue("@OldImage", oldPackage.Image);                    
+                    cmd.Parameters.AddWithValue("@OldPartner", oldPackage.PartnerURL);
+                    cmd.Parameters.AddWithValue("@OldAirfairInclusion", oldPackage.AirfairInclusion ? 1 : 0);
+                    cmd.Parameters.AddWithValue("@OldPkgStartDate", oldPackage.PackageStartDate);
+                    cmd.Parameters.AddWithValue("@OldPkgEndDate", oldPackage.PackageEndDate);
+                    cmd.Parameters.AddWithValue("@OldPkgDesc", oldPackage.PackageDescription);
+                    cmd.Parameters.AddWithValue("@OldPkgBasePrice", oldPackage.PackageBasePrice);
+                    cmd.Parameters.AddWithValue("@OldPkgAgencyCommission", oldPackage.PackageAgencyCommission);
+
+                    success = cmd.ExecuteNonQuery() > 0; // Success if rows changed
+
+                } // cmd object recycled
+            }// connection object recycled
+                        
+
+            return success;
+        }
+
+        /// <summary>
+        /// Deletes package specified only if all information is the same as in the program
+        /// </summary>
+        /// <param name="oldPackage">Package to delete</param>
+        /// <returns>Successful?</returns>
+        public static bool DeletePackage(Package oldPackage)
+        {
+            bool success = false;
+            // Opens connection
+            using (SqlConnection connection = TravelExpertsDB.GetConnection())
+            {
+                string query =
+                    "DELETE FROM Packages " +
+                    "WHERE " +
+                    "PackageId = @OldPackageId AND " +
+                    "PkgName = @OldPkgName AND " +
+                    "Image = @OldImage AND " +
+                    "Partner = @OldPartner AND " +
+                    "AirfairInclusion = @OldAirfairInclusion AND " +
+                    "PkgStartDate = @OldPkgStartDate AND " +
+                    "PkgEndDate = @OldPkgEndDate AND " +
+                    "PkgDesc = @OldPkgDesc AND " +
+                    "PkgBasePrice = @OldPkgBasePrice AND " +
+                    "PkgAgencyCommission = @OldPkgAgencyCommission ; ";
+                // Adds all parameters to new SQL Command
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+
+                    cmd.Parameters.AddWithValue("@OldPackageId", oldPackage.PackageId);
+                    cmd.Parameters.AddWithValue("@OldPkgName", oldPackage.PackageName);
+                    cmd.Parameters.AddWithValue("@OldImage", oldPackage.Image);
+                    cmd.Parameters.AddWithValue("@OldPartner", oldPackage.PartnerURL);
+                    cmd.Parameters.AddWithValue("@OldAirfairInclusion", oldPackage.AirfairInclusion ? 1 : 0);
+                    cmd.Parameters.AddWithValue("@OldPkgStartDate", oldPackage.PackageStartDate);
+                    cmd.Parameters.AddWithValue("@OldPkgEndDate", oldPackage.PackageEndDate);
+                    cmd.Parameters.AddWithValue("@OldPkgDesc", oldPackage.PackageDescription);
+                    cmd.Parameters.AddWithValue("@OldPkgBasePrice", oldPackage.PackageBasePrice);
+                    cmd.Parameters.AddWithValue("@OldPkgAgencyCommission", oldPackage.PackageAgencyCommission);
+
+                    success = cmd.ExecuteNonQuery() > 0; // Success if rows have been deleted
+
+                } // cmd object recycled
+            }// connection object recycled
+         
+
+            return success;
+        }
+
+        /// <summary>
+        /// Adds a new package to the DB
+        /// </summary>
+        /// <param name="newPackage">Package to add</param>
+        /// <returns>Successful?</returns>
+        public static bool AddPackage(Package newPackage)
+        {
+            bool success = false;
+            // Opens connection
+            using (SqlConnection connection = TravelExpertsDB.GetConnection())
+            {
+                string query = "INSERT INTO Packages VALUES " +
+                               $"(@PkgName,@Image,@Partner,@AirfairInclusion,@PkgStartDate,@PkgEndDate,@PkgDesc,@PkgBasePrice,@PkgAgencyCommission); ";
+                // Builds command with parameters
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+
+                    cmd.Parameters.AddWithValue("@PkgName", newPackage.PackageName);
+                    cmd.Parameters.AddWithValue("@Image", newPackage.Image);
+                    cmd.Parameters.AddWithValue("@Partner", newPackage.PartnerURL);
+                    cmd.Parameters.AddWithValue("@AirfairInclusion", newPackage.AirfairInclusion?1:0);
+                    cmd.Parameters.AddWithValue("@PkgStartDate", newPackage.PackageStartDate);
+                    cmd.Parameters.AddWithValue("@PkgEndDate", newPackage.PackageEndDate);
+                    cmd.Parameters.AddWithValue("@PkgDesc", newPackage.PackageDescription);
+                    cmd.Parameters.AddWithValue("@PkgBasePrice", newPackage.PackageBasePrice);
+                    cmd.Parameters.AddWithValue("@PkgAgencyCommission", newPackage.PackageAgencyCommission);
+
+                    success = cmd.ExecuteNonQuery()>0;
+                    
+                } // cmd object recycled
+            }// connection object recycled
+
+            
+            return success;
+        }
+
     }
 }
