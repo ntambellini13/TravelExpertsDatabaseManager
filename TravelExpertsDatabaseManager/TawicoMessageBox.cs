9db90046aac38067ef078e86ede49e7f33b44de1
owinayo <owinayo@gmail.com>
2019-10-03 07:59:47 -0600

    Fully Commented Tawico Message Box Class

--- TravelExpertsDatabaseManager/TawicoMessageBox.cs
+++ TravelExpertsDatabaseManager/TawicoMessageBox.cs
@@ -9,6 +9,14 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using TravelExpertsData;
 
+/*
+ * Purpose: Tawico Message Box Form Class loads and runs custom message box whenever called
+ * Replaced all message boxes with custom message box
+ * Author: Tawico 
+ * Date: September 18, 2019
+ * 
+ * */
+
 namespace TravelExpertsDatabaseManager
 {
     public partial class TawicoMessageBox : Form
@@ -18,6 +26,11 @@ namespace TravelExpertsDatabaseManager
             InitializeComponent();
         }
 
+        /// <summary>
+        /// When form loads set styling on various components
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void TawicoMessageBox_Load(object sender, EventArgs e)
         {
             //set back color of popup dialog
@@ -43,16 +56,29 @@ namespace TravelExpertsDatabaseManager
             }
         }
 
+        //Private boolean class variables
+        //Used to determine the type of message box by setting dialog result
+        //Used to configure button text
         private bool confirmYes = false;
         private bool confirmOk = false;
         private bool confirmNo = false;
         private bool confirmCancel = false;
 
+        /// <summary>
+        /// Custom Show method; used to open Tawico message box and display appropriate info/confirmations
+        /// </summary>
+        /// <param name="text">The string message to display in message box</param>
+        /// <param name="foreColour">default color for messages</param>
+        /// <param name="confirm">boolean used to configure buttons displayed</param>
+        /// <param name="cancel">boolean used to configure buttons displayed</param>
+        /// <returns></returns>
         public DialogResult Show(string text, Color foreColour, bool confirm = false,bool cancel = false)
         {
-            TawicoLabel.Text = text;
-            TawicoLabel.ForeColor = foreColour;
+            TawicoLabel.Text = text;//set the label text to input parameter text
+            TawicoLabel.ForeColor = foreColour;//set the label text color to input parameter
 
+            //default mode for message box; OK confirmation button
+            //shows and configures buttons according to method input parameters
             if (!confirm && !cancel)
             {
                 TawicoButtonCancel.Visible = false;
@@ -60,9 +86,12 @@ namespace TravelExpertsDatabaseManager
                 TawicoButtonConfirm.Text = "OK";
                 TawicoButtonCancel.Text = "";
 
+                //set class variable; used for determining what dialog result to return
                 confirmOk = true;
             }
 
+            //alternate mode for message box; YesNo confirmation buttons
+            //shows and configures buttons according to method input parameters
             if (confirm && !cancel)
             {
                 TawicoButtonCancel.Visible = true;
@@ -70,34 +99,49 @@ namespace TravelExpertsDatabaseManager
                 TawicoButtonConfirm.Text = "Yes";
                 TawicoButtonCancel.Text = "No";
 
+                //set class variables; used for determining what dialog result to return
                 confirmYes = true;
                 confirmNo = true;
             }
 
-            return this.ShowDialog();
+            return this.ShowDialog();//shows a for with the configuration specified by input parameters
         }
 
+        /// <summary>
+        /// Confirmation button click event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void TawicoButtonConfirm_Click(object sender, EventArgs e)
         {
+            //Sets dialog result based on button config on click
             if (confirmOk)
             {
                 DialogResult = DialogResult.OK;
             }
 
-            if(confirmYes)
+            //Sets dialog result based on button config on click
+            if (confirmYes)
             {
                 DialogResult = DialogResult.Yes;
             }
         }
 
+        /// <summary>
+        /// Cancel button click event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void TawicoButtonCancel_Click(object sender, EventArgs e)
         {
+            //Sets dialog result based on button config on click
             if (confirmCancel)
             {
                 DialogResult = DialogResult.Cancel;
             }
 
-            if(confirmNo)
+            //Sets dialog result based on button config on click
+            if (confirmNo)
             {
                 DialogResult = DialogResult.No;
             }
